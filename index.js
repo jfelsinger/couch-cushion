9c4b480b91d4fe015816ba6893c4facdd52a0384
JFelsinger <JFFelsinger@gmail.com>
2014-12-29 13:24:19 -0500

    Removed walk from index

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 var CouchCushion = require('./src'),
-    walk = require('./lib/walk');
-
-// require all included models here
-walk(__dirname + '/src/models', require);
 
 module.exports = exports = CouchCushion;
