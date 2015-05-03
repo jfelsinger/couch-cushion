b1c7693b2621b31beb16069249db4813f137d914
JFelsinger <JFFelsinger@gmail.com>
2015-05-02 21:42:36 -0400

    Added coverage reporting

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@
 
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
-    mocha = require('gulp-mocha');
+    mocha = require('gulp-mocha'),
+    istanbul = require('gulp-istanbul');
 
 
 function handle(error) {
@@ -24,20 +25,32 @@ gulp.task('lint', function() {
 });
 
 gulp.task('lint-tests', function() {
-    return gulp.src('test/**/*.js')
+    gulp.src('test/**/*.js')
         .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter('default'))
         .on('error', handle);
 });
 
-gulp.task('test', ['lint'], function() {
-    return gulp.src('test/**/*.js')
-        .pipe(mocha({ reporter: 'list' }))
-        .on('error', handle);
+gulp.task('test', ['lint'], function(cb) {
+    gulp.src([
+        'index.js',
+        'lib/**/*.js',
+        'src/**/*.js',
+    ])
+        .on('error', handle)
+        .pipe(istanbul())
+        .pipe(istanbul.hookRequire())
+        .on('finish', function() {
+            gulp.src('test/**/*.js')
+                .on('error', handle)
+                .pipe(mocha({ reporter: 'list' }))
+                .pipe(istanbul.writeReports())
+                .on('end', cb);
+        });
 });
 
 gulp.task('tr-test', function() {
-    return gulp.src('test/**/*.js')
+    gulp.src('test/**/*.js')
         .pipe(mocha({ reporter: 'list' }));
 });
 
