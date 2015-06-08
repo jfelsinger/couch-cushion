cb547f0d6219340114a778fa6224f154a1c2b7d0
JFelsinger <JFFelsinger@gmail.com>
2015-06-08 12:15:29 -0400

    Fix, register ModelList model by default

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ CouchCushion = new CouchCushion();
 
 // manually require included models here
 require('./src/models/list')(CouchCushion);
+require('./src/models/model-list')(CouchCushion);
 
 
 module.exports = exports = CouchCushion;
